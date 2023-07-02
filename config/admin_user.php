9018cd0928e7971041df4b0515990705bd1e7cfd
qurbaniemrahov <qurbaniemrah@outlook.com>
2023-07-02 08:12:23 +0400

    created admin user

--- config/admin_user.php
+++ config/admin_user.php
@@ -3,18 +3,22 @@
 include('connection.php');
 
 
+if($_SERVER['REQUEST_METHOD'] === 'POST') {
+$user_email = $_POST['user_email'];
+$user_password = password_hash($_POST['user_password'], PASSWORD_DEFAULT);
 $sql = "CREATE TABLE IF NOT EXISTS 
 admin_user (id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
             user_email VARCHAR(100) NOT NULL,
             user_password VARCHAR(100) NOT NULL
             )";
+}
+
 
 if ($conn->query($sql) === FALSE) {
     echo "Error creating table: " . $conn->errorInfo()[2];
 }
 
-$user_email = "info@aftklassik.com";
-$user_password = password_hash("", PASSWORD_DEFAULT); // Hash the password for security
+
 
 $sql = "INSERT INTO admin_user (user_email, user_password) VALUES ('$user_email', '$user_password')";
 
